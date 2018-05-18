ff1f44aecfc9aa8011b93f3a7e4c30ccd538bc1a
daSmith1105 <mustangdas@gmail.com>
2018-05-17 20:43:39 -0500

    resolved Test DB URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/library';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-library';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://admin:das1105@ds033477.mlab.com:33477/test-library';
 exports.PORT = process.env.PORT || 8080;
